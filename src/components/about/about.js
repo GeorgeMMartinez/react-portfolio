927e0b42850587e1948ae7b1c4e4e35fd0f31ebe
Matt Martinez <matt@Matts-MacBook-Pro.local>
2022-08-01 20:47:09 -0700

    added readme

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -4,7 +4,7 @@ import Grid from '@mui/material/Grid';
 export const about = () => {
   return (
     <>
-    <Grid justifyContent="center" alignItems="center">
+    <Grid container justifyContent="center" alignItems="center" padding={4} spacing={4}>
       <Grid item xs={12}>
         <p>Welcome, my name is George Martinez and I am currently studying full stack web development at UC Irvine. There, I 
           have learned HTML, CSS, and Javascript. I am looking to begin a career in web development using the techincal skills
